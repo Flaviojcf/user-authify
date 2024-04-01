54b0a423a074780f4baefa63fecf6ce17afb2845
flavio <flavi@DESKTOP-USTCPSC>
2024-04-01 20:08:51 -0300

    refactor: Add validation and properties

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,10 +1,12 @@
-import Entity from '../../../@shared/entity/entity.abstract'
+import BaseEntity from '../../../@shared/entity/entity.abstract'
+import UserValidatorFactory from '../factory/user.validator.factory'
+import NotificationError from '../notification/notification.error'
 
-export default class User extends Entity {
+export default class User extends BaseEntity {
   private readonly _name: string
   private readonly _email: string
   private readonly _password: string
-  private readonly _active: boolean
+  private _active: boolean
 
   constructor (id: string, name: string, email: string, password: string) {
     super()
@@ -12,7 +14,38 @@ export default class User extends Entity {
     this._name = name
     this._email = email
     this._password = password
+    this.validate()
 
-    // this.validate()
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors())
+    }
+  }
+
+  get name (): string {
+    return this._name
+  }
+
+  get email (): string {
+    return this._email
+  }
+
+  get password (): string {
+    return this._password
+  }
+
+  isActive (): boolean {
+    return this._active
+  }
+
+  activate (): void {
+    this._active = true
+  }
+
+  deactivate (): void {
+    this._active = false
+  }
+
+  public validate (): void {
+    UserValidatorFactory.create().validate(this)
   }
 }
