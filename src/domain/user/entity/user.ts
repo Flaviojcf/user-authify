66ef1002b6080797511d989d490901c872e46b5e
flavio <flavi@DESKTOP-USTCPSC>
2024-04-01 18:27:41 -0300

    feat: Create user entity and test file

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,3 +1,18 @@
-            function teste ():    number {
-  return 1
+import Entity from '../../../@shared/entity/entity.abstract'
+
+export default class User extends Entity {
+  private readonly _name: string
+  private readonly _email: string
+  private readonly _password: string
+  private readonly _active: boolean
+
+  constructor (id: string, name: string, email: string, password: string) {
+    super()
+    this._id = id
+    this._name = name
+    this._email = email
+    this._password = password
+
+    this.validate()
+  }
 }
