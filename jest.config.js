92d5963f472d79dd9079e59a71f1da4877c4e339
flavio <flavi@DESKTOP-USTCPSC>
2024-04-01 19:17:42 -0300

    test: Test workflow

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}'
-  ],
-  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest '
+  testMatch: ['<rootDir>/**/*.spec.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
   }
 }
