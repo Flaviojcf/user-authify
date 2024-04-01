4e02bb32322c7e62ba9e7033d94e6837595b4f3f
flavio <flavi@DESKTOP-USTCPSC>
2024-04-01 20:08:34 -0300

    feat: Add notification property

--- src/@shared/entity/entity.abstract.ts
+++ src/@shared/entity/entity.abstract.ts
@@ -1,5 +1,12 @@
-export default abstract class Entity {
+import Notification from '../../domain/user/notification/notification'
+
+export default abstract class BaseEntity {
   protected _id: string
+  public notification: Notification
+
+  constructor () {
+    this.notification = new Notification()
+  }
 
   get id (): string {
     return this._id
