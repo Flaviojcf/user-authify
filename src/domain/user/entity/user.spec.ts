ef8c332322652fcf6cd5ea0e7f1480c38b5b85a4
flavio <flavi@DESKTOP-USTCPSC>
2024-04-02 16:14:57 -0300

    refactor: Change name in test

--- src/domain/user/entity/user.spec.ts
+++ src/domain/user/entity/user.spec.ts
@@ -3,25 +3,25 @@ import User from './user'
 describe('User unit tests', () => {
   it('should throw error when id is empty', () => {
     expect(() => {
-      const user = new User('', 'John', 'John@gmail.com', 'password')
+      const user = new User('', 'Flavio', 'Flavio@gmail.com', 'password')
     }).toThrow('user: Id is required')
   })
 
   it('should throw error when name is empty', () => {
     expect(() => {
-      const user = new User('123', '', 'John@gmail.com', 'password')
+      const user = new User('123', '', 'Flavio@gmail.com', 'password')
     }).toThrow('user: Name is required')
   })
 
   it('should throw error when email is empty', () => {
     expect(() => {
-      const user = new User('123', 'John', '', 'password')
+      const user = new User('123', 'Flavio', '', 'password')
     }).toThrow('user: Email is required')
   })
 
   it('should throw error when password is empty', () => {
     expect(() => {
-      const user = new User('123', 'John', 'John@gmail.com', '')
+      const user = new User('123', 'Flavio', 'Flavio@gmail.com', '')
     }).toThrow('user: Password is required')
   })
 })
