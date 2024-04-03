14b198a2686c3cd20465be5b45ac7541a5d015d2
flavio <flavi@DESKTOP-USTCPSC>
2024-04-03 20:33:02 -0300

    feat: Create user repository interface

---
+++ src/domain/user/interfaces/repository/user.repository.interface.ts
@@ -0,0 +1,4 @@
+import type RepositoryInterface from '../../../@shared/interfaces/repository/repository.interface'
+import type User from '../../entity/user'
+
+export default interface UserRepositoryInterface extends RepositoryInterface<User> {}
