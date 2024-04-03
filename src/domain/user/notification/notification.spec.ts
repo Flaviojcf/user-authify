2f6d7a863785126579a4d7438cbbbfff9e2ce94d
flavio <flavi@DESKTOP-USTCPSC>
2024-04-03 19:42:30 -0300

    test: Create notification test

---
+++ src/domain/user/notification/notification.spec.ts
@@ -0,0 +1,66 @@
+import { ErrorConstants } from '../../@shared/utils/error-constants/error.constants'
+import Notification from './notification'
+
+describe('Unit test for notification', () => {
+  it('should create an error', () => {
+    const notification = new Notification()
+
+    const error = {
+      message: 'Id is required',
+      context: 'user'
+    }
+
+    notification.addError(error)
+
+    expect(notification.messages('user')).toBe(`${ErrorConstants.user.id},`)
+  })
+
+  it('should create errors', () => {
+    const notification = new Notification()
+
+    const error = {
+      message: 'Id is required',
+      context: 'user'
+    }
+
+    notification.addError(error)
+
+    expect(notification.messages('user')).toBe(`${ErrorConstants.user.id},`)
+
+    const error2 = {
+      message: 'Email is required',
+      context: 'user'
+    }
+    notification.addError(error2)
+
+    expect(notification.messages('user')).toBe(`${ErrorConstants.user.id},${ErrorConstants.user.email},`)
+  })
+
+  it('should check if notification has at least one error', () => {
+    const notification = new Notification()
+
+    const error = {
+      message: 'Email is required',
+      context: 'user'
+    }
+
+    notification.addError(error)
+
+    expect(notification.hasErrors()).toBe(true)
+  })
+
+  it('should get all errors', () => {
+    const notification = new Notification()
+
+    const error = {
+      message: 'Email is required',
+      context: 'user'
+    }
+
+    notification.addError(error)
+
+    expect(notification.getErrors().length).toBe(1)
+    expect(notification.getErrors()[0].message).toBe('Email is required')
+    expect(notification.getErrors()[0].context).toBe('user')
+  })
+})
