cf27d9b2125a75448728148d9d077d4a53f67883
flavio <flavi@DESKTOP-USTCPSC>
2024-04-06 14:32:04 -0300

    refactor: Update zod validate

--- src/domain/user/validator/user.zod.validator.ts
+++ src/domain/user/validator/user.zod.validator.ts
@@ -7,12 +7,14 @@ export default class UserZodValidator implements ValidatorInterface<User> {
   public validate (entity: User): void {
     try {
       const userSchema = zod.object({
-        name: zod.string({ required_error: ErrorConstants.user.name }),
-        email: zod.string({ required_error: ErrorConstants.user.email }),
-        password: zod.string({ required_error: ErrorConstants.user.password })
+        id: zod.string().uuid({ message: ErrorConstants.user.id }),
+        name: zod.string({ required_error: ErrorConstants.user.name }).trim().min(1, ErrorConstants.user.name),
+        email: zod.string({ required_error: ErrorConstants.user.email }).trim().min(1, ErrorConstants.user.email),
+        password: zod.string({ required_error: ErrorConstants.user.password }).trim().min(1, ErrorConstants.user.password)
       })
 
       userSchema.parse({
+        id: entity.id,
         name: entity.name,
         email: entity.email,
         password: entity.password
