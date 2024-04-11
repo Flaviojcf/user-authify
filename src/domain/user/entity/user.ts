40caf37105bacf2305fdd1bfaffecfef218d40a5
flavio <flavi@DESKTOP-USTCPSC>
2024-04-10 21:50:13 -0300

    feat: Add new property to user entity domain

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -7,14 +7,16 @@ export default class User extends BaseEntity {
   private readonly _email: string
   private readonly _password: string
   private _register: Date
+  private readonly _validation_id: string
 
-  constructor (id: string, name: string, email: string, password: string, register: Date) {
+  constructor (id: string, name: string, email: string, password: string, register: Date, validationId: string) {
     super()
     this._id = id
     this._name = name
     this._email = email
     this._password = password
     this._register = register
+    this._validation_id = validationId
     this.validate()
 
     if (this.notification.hasErrors()) {
@@ -36,6 +38,10 @@ export default class User extends BaseEntity {
 
   get register (): Date {
     return this._register
+  }
+
+  get validationId (): string {
+    return this._validation_id
   }
 
   isRegistered (): Date {
