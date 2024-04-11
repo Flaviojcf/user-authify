032e521934661b72c125aa6c08c27c13b0991790
flavio <flavi@DESKTOP-USTCPSC>
2024-04-10 21:48:36 -0300

    feat: Add new http method to route

--- src/presentation/user/rest/route/user.route.ts
+++ src/presentation/user/rest/route/user.route.ts
@@ -1,8 +1,11 @@
 import express, { type Request, type Response } from 'express'
 import CreateUserUseCase from '../../../../usecase/user/create/create.user.usecase'
 import UserRepository from '../../../../infrastructure/database/postgres/prisma/user/repository/user.repository'
+import FindUserUseCase from '../../../../usecase/user/find/find.user.usecase'
+import UpdateUserUseCase from '../../../../usecase/user/update/update.user.usecase'
 
 export const userRoute = express.Router()
+
 userRoute.post('/', async (request: Request, response: Response) => {
   const usecase = new CreateUserUseCase(new UserRepository())
 
@@ -20,5 +23,39 @@ userRoute.post('/', async (request: Request, response: Response) => {
     response.json(outputCreateUserDTO)
   } catch (err) {
     response.status(500).send(err)
+  }
+})
+
+userRoute.put('/confirm-email/:id', async (request: Request, response: Response) => {
+  const findUserUseCase = new FindUserUseCase(new UserRepository())
+  const updateUserUseCase = new UpdateUserUseCase(new UserRepository())
+
+  try {
+    const userId = request.params.id
+    const token = request.query.token
+
+    if (!token || typeof token !== 'string') {
+      return response.status(400).send('Invalid Token')
+    }
+
+    const user = await findUserUseCase.execute(userId)
+
+    if (user == null) {
+      return response.status(404).send('User not found')
+    }
+
+    const outputUpdateUserDTO = await updateUserUseCase.execute({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      isRegistered: new Date(),
+      validationId: user.validationId
+    })
+
+    response.json(outputUpdateUserDTO)
+  } catch (error) {
+    console.error(error)
+    response.status(500).send()
   }
 })
