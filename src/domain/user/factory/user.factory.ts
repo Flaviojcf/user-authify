965e93cdbbc195f3b17ad199f730703b962030c8
flavio <flavi@DESKTOP-USTCPSC>
2024-04-10 21:50:39 -0300

    refactor: Add new property to user factory

--- src/domain/user/factory/user.factory.ts
+++ src/domain/user/factory/user.factory.ts
@@ -2,7 +2,7 @@ import User from '../entity/user'
 import { v4 as uuid } from 'uuid'
 
 export default class UserFactory {
-  public static create (name: string, email: string, password: string): User {
-    return new User(uuid(), name, email, password, null)
+  public static create (name: string, email: string, password: string, isRegistered?: Date, validationId?: string): User {
+    return new User(uuid(), name, email, password, isRegistered ?? null, validationId ?? uuid())
   }
 }
